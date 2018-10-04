0dfa737d8a01ff6cb4a0a3affd62d5934d4876c1
OmegaHelix <dakotaf@sasktel.net>
2018-10-04 15:46:25 -0600

    Generated Identity Pages

---
+++ Source/eIdeas/eIdeas/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using eIdeas.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(eIdeas.Areas.Identity.IdentityHostingStartup))]
+namespace eIdeas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
