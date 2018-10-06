804f71f341290c819dd918e69e77902e712b7df4
Xooma0610 <christianjohn22@rocketmail.com>
2018-10-06 15:29:48 -0600

    changes to Register and Contact
    Made changes to the Register page to include username, phone number and image. Also a few modifications to the Contact page.

--- Source/eIdeas/eIdeas/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Source/eIdeas/eIdeas/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,6 +45,22 @@ namespace eIdeas.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [Phone]
+            [DataType(DataType.PhoneNumber)]
+            [Display(Name = "PhoneNumber")]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [Url]
+            [DataType(DataType.ImageUrl)]
+            [Display(Name = "Image")]
+            public string Image { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
