372d94c3af225e7dc98611935c1a98657aa3f681
OmegaHelix <dakotaf@sasktel.net>
2018-10-10 18:46:41 -0600

    Added Security

--- Source/eIdeas/eIdeas/Migrations/20181011000528_Initial.Designer.cs
+++ Source/eIdeas/eIdeas/Migrations/20181011000528_Initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eIdeas.Data;
 
 namespace eIdeas.Migrations
 {
     [DbContext(typeof(IdeasContext))]
-    partial class IdeasContextModelSnapshot : ModelSnapshot
+    [Migration("20181011000528_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/eIdeas/eIdeas/Migrations/20181011000528_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eIdeas.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Idea",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Problem = table.Column<string>(nullable: true),
+                    Solution = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    UploadDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Idea", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Idea");
+        }
+    }
+}
