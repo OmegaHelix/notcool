7dfd37aced22279395189c0a917460fe6e0d642b
OmegaHelix <dakotaf@sasktel.net>
2018-10-11 15:09:38 -0600

    added usm milestone 2

--- Source/eIdeas/eIdeas/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Source/eIdeas/eIdeas/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -83,7 +83,7 @@ namespace eIdeas.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Username, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
